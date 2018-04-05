5abf3a38af56fc5eb24a8eaebf4b4ac57f8bb0f4
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-04-06 01:58:55 +0800

    fix bug: the list parsing results do not match the standard syntax.

--- src/list.js
+++ src/list.js
@@ -5,47 +5,148 @@ import splitBlock from './utils/splitBlock';
 
 const { vnode } = nodes;
 
-const parseList = lines => {
-  const patt = /^ {0,3}([*+-]|\d+\.)(( +)(.*)((\n *[^*+-\s].*)|(\n\n? \3.*))*)/mg
-  const list = [];
+const clearEnter = content => {
+  const matched = /\n(\s*\n)*$/g.exec(content);
+  if (!matched) return undefined;
+
+  return matched[1]
+}
+
+const paragraph = str => {
+  const patt = /(^.*\S.*$)+/m
+}
+
+const blockPara = (str, prefix) => {
+  const patt = new RegExp(`^(\\s*\\n)+(${prefix}.*\\S.*(\\n|$))?`)
+
+  const matched = patt.exec(str)
+  if (!matched) return { pass: false }
+
+  if (matched[2]) return { pass: true, value: matched[0] }
+
+  return { pass: true, value: matched[0], end: true }
+}
+
+const inlinePara = (str) => {
+  const patt = /^.*\S.*(\n|$)/
+
+  const matched = patt.exec(str)
+  if (!matched) return { pass: false }
+
+  return { pass: true, value: matched[0] }
+}
+
+const content = (str, prefix) => {
+  const stack = []
+  const patt = /^ {0,3}([*+-]|(\d+.)) +/
 
   while (true) {
-    const next = patt.exec(lines);
-    if (!next) break;
+    if (patt.exec(str)) break;
 
-    let [, prefix, content] = next;
-    let li$;
-    content = content.replace(/^ {0,4}/mg, '');
+    const iResult = inlinePara(str, prefix)
 
-    if (/\n/.test(content)) {
-      li$ = vnode('li', [block(content)]);
-    } else {
-      const inline$ =
-      li$ = vnode('li', [inline(content)]);
+    if (iResult.pass) {
+      stack.push(iResult.value)
+      str = str.substr(iResult.value.length)
+      continue
     }
 
-    list.push(li$);
+    const bResult = blockPara(str, prefix)
+
+    if (bResult.pass) {
+      stack.push(bResult.value)
+      str = str.substr(bResult.value.length)
+
+      if (!bResult.end) continue
+    }
+
+    break;
   }
 
-  return list;
-};
+  return stack.join('');
+}
 
+const matchList = (str, patt) => {
+  // const patt = /^( {0,3})([*+-])( +)/
+  const stack = []
+
+  while (true) {
+    const matched = patt.exec(str)
+    if (!matched) break;
+    const [all] = matched
+    const prefix = new Array(all.length).fill(' ').join('')
+    str = str.substr(all.length)
+
+    const value = content(str, prefix)
+    stack.push({
+      content: value.replace(new RegExp(`^${prefix}`, 'mg'), ''),
+      length: all.length + value.length,
+    })
+    str = str.substr(value.length)
+  }
+
+  const last = stack[stack.length - 1]
+  const emptyLines = clearEnter(last.content)
+
+  if (emptyLines) {
+    last.content = last.content.substr(0, last.content.length - emptyLines.length)
+    last.length = last.length - emptyLines.length
+  }
+
+  console.log(stack)
+  return stack
+    .map(item => ({
+      ...item,
+    content: /.*\n.*\n/.test(item.content) ?
+      block(item.content) : inline(item.content.replace(/\n$/, '')),
+    }))
+    .map(item => ({
+      length: item.length,
+      node: vnode('li', [item.content])
+    }))
+    // .map(item => {
+    //   console.log(item.node.children)
+    //   return item
+    // })
+}
+
+const findList = str => {
+  const stack = []
+
+  while (true) {
+    const matched = /^( {0,3})([*+-]|(\d+.)) /mg.exec(str)
+    if (!matched) break;
+
+    if (matched.index !== 0) {
+      stack.push(block(str.substr(0, matched.index)))
+      str = str.substr(matched.index)
+    }
+
+    let list = null;
+    if (matched[3]) {
+      list = matchList(str, /^( {0,3})(\d+.)( +)/)
+      stack.push(vnode('ol', list.map(item => item.node)))
+    } else {
+      list = matchList(str, /^( {0,3})([*+-])( +)/)
+      stack.push(vnode('ul', list.map(item => item.node)))
+    }
+
+    const length = list
+      .reduce((sum, item) => sum + item.length, 0)
+    str = str.substr(length)
+  }
+
+  if (stack.length && str.length) stack.push(block(str))
+
+  return stack;
+}
 
 export default middleware({
   name: 'list',
   input: 'block',
   parse: node => {
-    const patt = /^( {0,3}([*+-]|\d+\.)( +)(.*)((\n *[^*+-\s].*)|(\n\n? \3.*))*(?:\n|$))+/mg
-    const group = splitBlock(node, patt, matched => {
-      const [list, prefix] = matched;
-      const items = parseList(list);
-      let list$;
-
-      if (/[*+-]/.test(prefix)) list$ = vnode('ul', items);
-      else list$ = vnode('ol', items);
-
-      return list$;
-    });
+    const group = findList(node.text)
+    console.log('group: ', group)
 
     if (group.length) return group;
     return node;
