fec83fbab1e3ca1f08931724fe8abd75f4ba6d45
Val.istar.Guo <Val.istar.Guo@gmail.com>
2018-08-31 23:27:58 +0800

    text ast node never need encode html

--- src/nodes/text.js
+++ src/nodes/text.js
@@ -1,9 +1,6 @@
-import { htmlEncode } from 'md-core/utils';
-
-
 export default node => value => ({
   ...node('text', value),
   parse() {
-    return htmlEncode(this.value)
+    return this.value
   }
 })
